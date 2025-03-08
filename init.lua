6a8631cd9849e1741ef0f9899171a62f5191fe1e
ra <ra@example.com>
2025-03-08 09:20:35 +0200

    feat: add undo history and whitespace trimming
    - Added undo settings in `init.lua` to enable persistent undo and set the
      undo directory.
    - Configured recursive search for `:find` command in `init.lua`.
    - Updated lazy.nvim bootstrap code in `init.lua` for consistency.
    - Added new file `after/ftplugin/lua.lua` to set local options for Lua files.
    - Updated `lazy-lock.json` with new plugin versions.
    - Added keymapping in `lua/keymappings.lua` to trim whitespaces and edit
      alternate file.

--- init.lua
+++ init.lua
@@ -4,42 +4,48 @@
 vim.o.splitright = true
 vim.o.splitbelow = true
 
+-- undo settings
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath("data") .. "/undo"
+
+-- recursive search for :find
+vim.o.path = vim.o.path .. "**"
+
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
-require('lazy').setup {
-  spec = {
-    -- import your plugins
-    { import = 'plugins' },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { 'habamax' } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-}
+require("lazy").setup({
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
 
 -- Keymappings are defined in lua/keymappings.lua
-require('keymappings')
-
+require("keymappings")
--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,1 +1,2 @@
 vim.keymap.set('n', '<leader>W', ":lua require('whitespace-nvim').trim()<CR>", { desc = 'Trim whitespaces' })
+vim.keymap.set('n', '<leader><space>', "<C-^>", { desc = 'Edit alternate file' })
