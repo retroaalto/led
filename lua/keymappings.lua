a57d955bc25d5bbe5de0efc52a1f38edb205fea8
ra <ra@example.com>
2025-03-08 13:20:38 +0200

    feat(telescope): add telescope plugin and key mappings
    - Added `telescope.nvim` plugin to the project.
    - Updated `lazy-lock.json` to include the new plugin.
    - Modified `lua/keymappings.lua` to add key mappings for Telescope:
      - `<leader>sf` to find files
      - `<leader>sg` for live grep
      - `<leader>sb` to list buffers
      - `<leader>sh` to show help tags
    - Created `lua/plugins/telescope.lua` to configure the plugin with
      dependencies.

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,2 +1,8 @@
-vim.keymap.set('n', '<leader>W', ":lua require('whitespace-nvim').trim()<CR>", { desc = 'Trim whitespaces' })
-vim.keymap.set('n', '<leader><space>', "<C-^>", { desc = 'Edit alternate file' })
+vim.keymap.set("n", "<leader>W", ":lua require('whitespace-nvim').trim()<CR>", { desc = "Trim whitespaces" })
+vim.keymap.set("n", "<leader><space>", "<C-^>", { desc = "Edit alternate file" })
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Telescope help tags" })
